1d0d727a70d309a69315c66d1e36a27500fb0de4
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-25 12:03:28 +0600

    sum of all odd number

--- problem1.js
+++ problem1.js
@@ -1,2 +1,17 @@
 // palindrome number -----------
 
+function getReverseString (str){
+    var reverseString = " ";
+    for(let i=str.length-1; i>=0; i--){
+        reverseString += str[i];
+        if (reverseString === string){
+            console.log('palindrome');
+        }
+        else{
+            console.log('not palindrome');
+        }
+    }
+    return reverseString;
+}
+const string =getReverseString('mom');
+// console.log(string);
---
+++ problem2.js
@@ -0,0 +1,34 @@
+// Write a function that takes in an array of integers and returns the sum of all the odd numbers in the array.----------
+
+function getSumOfArray(numbers){
+    let sum = 0;
+    for (let i=0; i<numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        // console.log(element);
+        sum = sum + element;
+        console.log(sum);
+    }
+    return sum;
+}
+
+function getOddNumbers(numbers){
+    let oddNumbers = [];
+    for(let i = 1; i<numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        
+        if( element%2 !== 0){
+            oddNumbers.push(element);
+            // console.log(element,index);
+        }
+    }
+    return oddNumbers;
+}
+
+
+const myNumbers = [21,43,35,45,56,65,47,78,58,49,49];
+// getSumOfArray(myNumbers);
+const oddNumbers = getOddNumbers(myNumbers);
+// console.log(oddNumbers);
+const oddNumberSum = getSumOfArray(oddNumbers);
