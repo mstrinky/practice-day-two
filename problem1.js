c5d3bcaae5b12f283b158499446be387fcb044f0
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-25 23:24:14 +0600

    palindrome problem solved

--- problem1.js
+++ problem1.js
@@ -2,17 +2,11 @@
 // Write a function that takes in a string and returns true if it's a palindrome, and false otherwise.----------
 
 function getPalindrome(str){
-    let palindrome = ' ';
-    for (let i=str.length-1; i>=0; i--){
-        const element = str[i];
-        palindrome = palindrome + element;
-        
-        
-       
-    }
-    return palindrome;
+    let lowercaseStr = str.toLowerCase();
+    let reverseString = lowercaseStr.split('').reverse().join('');
+  
+    return (lowercaseStr === reverseString);
 }
 
-const string = 'mom';
-const palindromeNumber = getPalindrome(string);
-console.log(palindromeNumber);
+
+console.log(getPalindrome('madam'));
---
+++ problem5.js
@@ -0,0 +1,2 @@
+// Write a function that takes in an array of integers and returns the second smallest number in the array.------
+
