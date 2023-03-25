2a6fd4ae2d83f25e94e9cf002564dbd6500316fc
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-25 22:03:12 +0600

    new array

---
+++ problem4.js
@@ -0,0 +1,5 @@
+// Write a function that takes in two arrays and returns a new array that contains all the elements that are in both arrays.--
+var fruitName = ['apple','orange','banana','lichi'];
+var flowerName = ['rose','waterlily','sunflower','marigold'];
+var add = fruitName.concat(flowerName);
+console.log(add);
