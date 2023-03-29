677a0a9db67fe0c070297e25f01759d83ac11859
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-29 22:36:58 +0600

    find longest number

--- problem5.js
+++ problem5.js
@@ -1,2 +1,11 @@
 // Write a function that takes in an array of integers and returns the second smallest number in the array.------
 
+function secondSmallest(arr) {
+    // sort array in ascending order
+    arr.sort((a, b) => a - b);
+    // return second element in array
+    return arr[1];
+  }
+  
+  console.log(secondSmallest([1, 2, 3])); // 2
+  console.log(secondSmallest([5, 4, 3, 2, 1])); // 2
--- problem7.js
+++ problem7.js
@@ -1,1 +1,13 @@
 // Write a function that takes in an array of strings and returns the longest string in the array. 
+
+function findLongestString(str){
+    let longestNumber =" ";
+    for (let i=0; i<str.length; i++){
+        if(str[i].length > longestNumber.length){
+            longestNumber = str[i];
+        }
+    }
+    return longestNumber;
+}
+
+console.log(findLongestString(['rinky','jesmin','salu']));
